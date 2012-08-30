4f2bc75aa88c80b54f81163164b35701af16c8db
gfloresuth <dexterminio@gmail.com>
2012-08-29 21:22:21 -0700

    Better documentation look.

--- js/jscanvaslib.js
+++ js/jscanvaslib.js
@@ -1,16 +1,20 @@
 /*
-Created by: Gustavo Adolfo Flores González
-
-Project: JSCanvasLib
-File: js/jscanvaslib.js
-Description: Is an easy to use JavaScript Library for the Canvas element (html5).
-
-
+* Created by: Gustavo Adolfo Flores González
+* Project: JSCanvasLib
+* File: js/jscanvaslib.js
+* Description: Is an easy to use JavaScript Library for the Canvas element (html5).
+* 
+* Last modified: 29 August 2012
+* 
 */
 
-// DOMTool is a simple DOM object manager
 
-// AsyncImage Object (Handles all the async loading and drawing of the image)
+/*
+*
+* Object: AsyncImage (Handles all the async loading and drawing of the image)
+* @param aPath Path to the image
+*
+*/
 function AsyncImage(aPath,nPars,callback)
 {
 	// the real image
@@ -37,26 +41,61 @@ function AsyncImage(aPath,nPars,callback)
 	return odata;
 }
 
+/*
+* Function isset Checks if a variabl is set to a value
+* @param aVar A viariable to check
+*
+*/
 function isset(aVar)
 {
 	return (typeof aVar == "undefined")?false:true;
 }
 
-// "The most important object of all" (ok, is the main object JSCanvasLib)
+/*
+*
+* Object: JSCanvasLib The main Object of this project
+*
+* Note: "The most important object of all" (ok, is the main object JSCanvasLib)
+*
+*/
 function JSCanvasLib(){
 	// creates the internal object in JSON style
 	var odata={
-		// about (a little ego in here)
+		/*
+		* about Shows the about dialog
+		* Note: (a little ego in here)
+		*
+		*/
 		about:function(){alert("JSCanvas 1.0\nby Gustavo Adolfo Flores González")},
 		
-		// Context reference of the canvas
+		/*
+		* Context reference of the canvas
+		*
+		*/
 		ctx:null,
 		
-		// This is the image list (used to save image instances).
+		/*
+		* This is the image list (used to save image instances).
+		*
+		*/
 		_images:{},
 		
-		// The magic function _asyncDrawImage
-		// Description: This functions gets the parameters from the AsyncImage object, and do the actual drawing
+		/*
+		* The magic function _asyncDrawImage
+		* Description: This functions gets the parameters from the AsyncImage object, and do the actual drawing
+		* @param data The basic data for the image (the actual parameters)
+		*
+		* Description
+		*  This are the true parameters:
+		* data.img Image path
+		* data.pars.x Parameter X
+		* data.pars.y Parameter Y
+		* data.pars.w Parameter w (Width)
+		* datapars.h Parameter h (Height)
+		* 
+		* Note: When data.pars.w and data.pars.h are set to a value, it uses the full drawImage.
+		*
+		*/
 		_asyncDrawImage:function(data){
 			//data.pars is the actual parameters when drawImage was called (url,ix,iy)
 			//data.pars.x is the x position
@@ -71,41 +110,88 @@ function JSCanvasLib(){
 			
 		},
 		
-		// This function do the initial async creation of the image calling
+		/*
+		* This function do the initial async creation of the image calling
+		* 
+		* @param url The url to the image (local or web based path)
+		* @param ix X coordinate
+		* @param iy Y coordinate
+		*/
 		drawImage:function(url,ix,iy){
 			
 			this._images[url]=new AsyncImage(url,{x:ix,y:iy,ctx:this.ctx},this._asyncDrawImage);
 		},
 
-		// This function do the initial async creation of the image calling (with Stretch)
+		/*
+		* This function do the initial async creation of the image calling (with Stretch)
+		*
+		* @param url The url to the image (local or web based path)
+		* @param ix Initial X coordinate
+		* @param iy Initial Y coordinate
+		* @param iw Initial Width
+		* @param ih Initial Height
+		*
+		*/
 		drawImageStretch:function(url,ix,iy,iw,ih){
 			
 			this._images[url]=new AsyncImage(url,{x:ix,y:iy,w:iw,h:ih,ctx:this.ctx},this._asyncDrawImage);
 		},
 
 		
-		// Set the reference to the canvas object from DOM
+		/*
+		* Set the reference to the canvas object from DOM
+		*
+		* @param id The id of the canvas object (DOM based)
+		*
+		*/
 		setCanvas:function(id){
 				this.ctx=document.getElementById(id).getContext('2d');
 			},
 			
-		// This maps the property fillStyle to a function
+		/*
+		* This maps the property fillStyle to a function
+		*
+		* @param aColor Is the actual color
+		*
+		*/
 		setColor:function(aColor){
 				this.ctx.fillStyle=aColor;
 			},
 			
-		// This maps the property font to a function
+		/*
+		* This maps the property font to a function
+		*
+		* @param aFont Current font to set
+		*
+		*/
 		setFont:function(aFont){
 				this.ctx.font=aFont;
 			},
 		
-		// This function combines this.setColor and this.ctx.fillText to get a simple call to draw a string with color
+		/*
+		* This function combines this.setColor and this.ctx.fillText to get a simple call to draw a string with color
+		*
+		* @param text The texto to write
+		* @param x X coordinate
+		* @param y Y coordinate
+		* @param aColor Color of the string to draw
+		*
+		*/
 		drawString:function(text, x,y,aColor){
 				this.setColor(aColor);
 				this.ctx.fillText(text,x,y);
 			},
 		
-		// This function combines this.setColor and this.ctx.fillRect to draw a rectangle with color
+		/*
+		* This function combines this.setColor and this.ctx.fillRect to draw a rectangle with color
+		*
+		* @param x X coordinate
+		* @param y Y coordinate
+		* @param w Width
+		* @param h Height
+		* @param aColor The color to fill the Rectangle
+		*
+		*/
 		fillRect:function(x,y,w,h,aColor){
 				this.setColor(aColor);
 				this.ctx.fillRect(x,y,w,h);
@@ -115,4 +201,5 @@ function JSCanvasLib(){
 	return odata;
 	
 }
+
 
