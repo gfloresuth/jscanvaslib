7daf0821a9160fef8c2c95ca9c2f1ed52c0664f7
gfloresuth <dexterminio@gmail.com>
2014-09-13 20:40:06 +0000

    Small change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*
-* Created by: Gustavo Adolfo Flores González
+* Created by: Gustavo Adolfo Flores Gonzï¿½lez
 * Project: JSCanvasLib
 * File: js/main.js
 * Description: Is an easy to use JavaScript Library for the Canvas element (html5).
@@ -33,7 +33,7 @@ function main()
 	canv.fillRect(50,50,100,100,"rgba(0,0,200,0.5)");
 	
 	// Draws the clasic "Hello World" in white 
-	canv.drawString("Hello World",200,200,"white");
+	canv.drawString("Hello World, 2014",200,200,"white");
 	
 	// Loads and draws a simple image
 	canv.drawImage("images/test1.png",20,20);
